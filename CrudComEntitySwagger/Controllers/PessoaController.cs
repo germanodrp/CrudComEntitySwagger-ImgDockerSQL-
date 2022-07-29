9b4a32d37780c4eba5380fec1aad077ded740269
Germano Pereira <germano.pereira@bit.com.br>
2022-07-29 11:28:14 -0300

    Slides e Finalização v1 CRUD

--- CrudComEntitySwagger/Controllers/PessoaController.cs
+++ CrudComEntitySwagger/Controllers/PessoaController.cs
@@ -34,29 +34,53 @@ namespace CrudComEntitySwagger.Controllers
             return Ok(buscarPessoa);
         }
 
-      /*  [HttpPost]
+        [HttpPost]
         [Route("AdicionarPessoa")]
 
-        public async Task<ActionResult> AdicionarPessoa([FromBody]Pessoa pessoa)
+        public async Task<ActionResult<String>> AdicionarPessoa([FromBody]Pessoa pessoa)
         {
-           
+            _context.Pessoas.Add(pessoa);
+            _context.SaveChanges();
+
+            return Ok("Pessoa Adicionada!!");
+        }
+      
+       
+
+        [HttpPut]
+        [Route("AtualizarPessoa/{id}")]
+
+        public async Task<ActionResult> AtualizarPessoa(Pessoa pessoa,int id)
+        {
+
+            if(id != pessoa.Id)
+            {
+                return BadRequest("Id não encontrado");
+            }
+
+          
+            _context.Pessoas.Update(pessoa);
+            _context.SaveChanges();
+
+            return Ok("Atualizado com sucesso!");
+
         }
 
         [HttpDelete]
-        [Route("DeletarPessoa")]
+        [Route("DeletarPessoa/{id}")]
 
-        public async Task DeletarPessoa(int id )
+        public async Task<ActionResult<Pessoa>> DeletarPessoa(int id)
         {
+            if (id == null)
+            {
+                return BadRequest("Id não encontrado!! ");
+            }
+            var buscarPessoaPorId = _context.Pessoas.Find(id);
+            _context.Pessoas.Remove(buscarPessoaPorId);
+            _context.SaveChanges();
 
+            return Ok(buscarPessoaPorId);
         }
-
-        [HttpPut]
-        [Route("AtualizarPessoa")]
-
-        public async Task AtualizarPessoa(int id)
-        {
-
-        }*/
 
     }
 }
